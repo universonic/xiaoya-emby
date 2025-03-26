9deaa8df47a146a3221e0a75239454c6047f26ee
Alfred Zhou <unioverlord@gmail.com>
2025-03-26 17:01:55 +0800

    minor fix.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ ALIST_URL=${ALIST_URL:-"http://xiaoya.host:5678"}
 RUN_CRON_EXPR=${RUN_CRON_EXPR:-"0 0 * * *"}
 
 
-/app/bin/xiaoya-emby -r ${ALIST_STRM_ROOT_PATH} -u ${ALIST_URL} --cron-expr ${RUN_CRON_EXPR} "$@"
+/app/bin/xiaoya-emby -r "${ALIST_STRM_ROOT_PATH}" -u "${ALIST_URL}" --cron-expr "${RUN_CRON_EXPR}" "$@"
